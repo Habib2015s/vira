9d22340e4f8245041fa3e3a50c579daa5b7ca34a
red pc <habibbarca2015@gmail.com>
2026-08-02 23:35:39 +0330

    ready for run

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    trailingSlash: true,
-};
 
-export default nextConfig;
+
+    images: {
+        unoptimized: true,
+    },
+}
+
+module.exports = nextConfig
